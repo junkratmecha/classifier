65b9fac2245c550f8534a9bccd0efc3a8f485c94
junkratmecha <junkratmecha@gmail.com>
2022-09-20 00:07:02 +0900

    一旦music回り捨てる

--- app.py
+++ app.py
@@ -22,31 +22,6 @@ def index():
 def cnn():
     return render_template("cnn.html")
 
-@app.route("/rnn", methods=["GET", "POST"])
-def rnn():
-    if request.method == "GET":
-
-        seed = music_pb2.NoteSequence()  # NoteSequence
-
-        # notesにnoteを追加
-        seed.notes.add(pitch=80, start_time=0.0, end_time=0.4, velocity=80)
-        seed.notes.add(pitch=80, start_time=0.4, end_time=0.8, velocity=80)
-        seed.notes.add(pitch=87, start_time=0.8, end_time=1.2, velocity=80)
-        seed.notes.add(pitch=87, start_time=1.2, end_time=1.6, velocity=80)
-        seed.notes.add(pitch=89, start_time=1.6, end_time=2.0, velocity=80)
-        seed.notes.add(pitch=89, start_time=2.0, end_time=2.4, velocity=80)
-        seed.notes.add(pitch=87, start_time=2.4, end_time=3.2, velocity=80)
-
-        seed.total_time = 3.2  # 所要時間
-        seed.tempos.add(qpm=75);  # 曲のテンポを指定
-
-        graph = note_seq.plot_sequence(seed)  # NoteSequenceの可視化
-        music = note_seq.play_sequence(seed, synth=note_seq.fluidsynth)  # NoteSequenceの再生
-        
-        return render_template("rnn.html", graph=graph, music=music)
-    else:
-        return redirect(url_for("index"))
-
 @app.route("/result", methods=["GET", "POST"])
 def result():
     if request.method == "POST":
